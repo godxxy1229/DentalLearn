0dec79a43745b39a31adde9e2a8d1587683e9ca4
jongman <61715265+godxxy1229@users.noreply.github.com>
2024-11-07 20:14:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function evaluateAnswer(answer) {
   `;
 
   // Gemini API 요청
-  fetch(`https://generativelanguage.googleapis.com/v1beta2/models/${model}:generateContent?key=${apiKey}`, {
+  fetch(`https://cors-anywhere.herokuapp.com/https://generativelanguage.googleapis.com/v1beta2/models/${model}:generateContent?key=${apiKey}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
